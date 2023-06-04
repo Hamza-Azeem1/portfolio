ed3b14f091c2840f7cd5899d241c5c3b457a589b
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-06-04 16:51:32 +0500

    fixed

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,6 +3,14 @@ import Projects from "./projects";
 import DeveloperImg from "../assets/developer.svg";
 
 const Hero = () => {
+
+    const handleDownload = () => {
+        // Logic to handle the file download
+        // For example, you can use window.open or a file download library
+        window.open('/public/Cv.pdf', '_blank');
+    };
+
+
     return (
         <>
             <div>
@@ -14,7 +22,7 @@ const Hero = () => {
                         <p className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-300">
                             A Full-Stack Developer & Design Enthusiast
                         </p>
-                        <div className="flex justify-center sm:block">
+                        {/* <div className="flex justify-center sm:block">
                             <a
                                 download=""
                                 href="/public/Cv.pdf"
@@ -26,6 +34,18 @@ const Hero = () => {
                                     Download CV
                                 </span>
                             </a>
+                        </div> */}
+                        <div className="flex justify-center sm:block">
+                            <button
+                                className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
+                                onClick={handleDownload}
+                                aria-label="Download Resume"
+                            >
+                                <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100" />
+                                <span className="text-sm sm:text-lg font-general-medium duration-100">
+                                    Download CV
+                                </span>
+                            </button>
                         </div>
                     </div>
                     <div className="w-full sm:w-2/3 text-right float-right mt-8 sm:mt-0">
