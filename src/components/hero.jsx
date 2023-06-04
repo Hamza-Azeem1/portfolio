0e87db7f8b0bc4252d08fdf40e524ac7dc0496bf
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-06-04 17:52:31 +0500

    button updated

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,6 +3,11 @@ import Projects from "./projects";
 import DeveloperImg from "../assets/developer.svg";
 
 const Hero = () => {
+
+    const handleClick = () => {
+        window.open("https://drive.google.com/file/d/1MYl3G0veD9kCIIMJhblz8XmSD2wYD9qv/view?usp=sharing");
+    }
+
     return (
         <>
             <div>
@@ -16,14 +21,13 @@ const Hero = () => {
                         </p>
                         <div className="flex justify-center sm:block">
                             <button
+                                onClick={handleClick}
                                 className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
                             >
                                 <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100" />
-                                <a href="https://drive.google.com/file/d/1MYl3G0veD9kCIIMJhblz8XmSD2wYD9qv/view?usp=sharing" download="Hamza Resume" >
-                                    <span className="text-sm sm:text-lg font-general-medium duration-100">
-                                        Download CV
-                                    </span>
-                                </a>
+                                <span className="text-sm sm:text-lg font-general-medium duration-100">
+                                    Download CV
+                                </span>
                             </button>
                         </div>
                     </div>
