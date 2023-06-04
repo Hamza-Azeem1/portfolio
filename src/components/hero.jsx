160516875c0ecdfe32b3b7a5af9bb1e0afd3a9d1
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-06-04 17:40:12 +0500

    updated

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,17 +1,8 @@
 import { FiArrowDownCircle } from 'react-icons/fi';
 import Projects from "./projects";
 import DeveloperImg from "../assets/developer.svg";
-import { saveAs } from 'file-saver';
 
 const Hero = () => {
-
-    const handleDownload = () => {
-        const fileUrl = '/public/Hamza Resume.pdf';
-        const fileName = 'Hamza Resume';
-        saveAs(fileUrl, fileName);
-    };
-
-
     return (
         <>
             <div>
@@ -26,13 +17,13 @@ const Hero = () => {
                         <div className="flex justify-center sm:block">
                             <button
                                 className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
-                                onClick={handleDownload}
-                                aria-label="Download Resume"
                             >
                                 <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100" />
-                                <span className="text-sm sm:text-lg font-general-medium duration-100">
-                                    Download CV
-                                </span>
+                                <a href="/public/Hamza Resume.pdf" download="Hamza Resume" >
+                                    <span className="text-sm sm:text-lg font-general-medium duration-100">
+                                        Download CV
+                                    </span>
+                                </a>
                             </button>
                         </div>
                     </div>
