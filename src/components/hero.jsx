659aa598dfb7fc3f4c01b6069d136c5bfba630c7
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-06-04 17:34:56 +0500

    updated

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -23,19 +23,6 @@ const Hero = () => {
                         <p className="font-general-medium mt-4 text-lg md:text-xl lg:text-2xl xl:text-3xl text-center sm:text-left leading-normal text-gray-300">
                             A Full-Stack Developer & Design Enthusiast
                         </p>
-                        {/* <div className="flex justify-center sm:block">
-                            <a
-                                download=""
-                                href="/public/Cv.pdf"
-                                className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
-                                aria-label="Download Resume"
-                            >
-                                <FiArrowDownCircle className="mr-2 sm:mr-3 h-5 w-5 sn:w-6 sm:h-6 duration-100"></FiArrowDownCircle>
-                                <span className="text-sm sm:text-lg font-general-medium duration-100">
-                                    Download CV
-                                </span>
-                            </a>
-                        </div> */}
                         <div className="flex justify-center sm:block">
                             <button
                                 className="font-general-medium flex justify-center items-center w-36 sm:w-48 mt-12 mb-6 sm:mb-0 text-lg border border-indigo-200 py-2.5 sm:py-3 shadow-lg rounded-lg bg-indigo-50 focus:ring-1 focus:ring-indigo-900 hover:bg-indigo-500 text-gray-500 hover:text-white duration-500"
