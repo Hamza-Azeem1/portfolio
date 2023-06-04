6b73bc2a00042e75703193d0313add42e9fd270f
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-06-04 16:58:12 +0500

    routes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,25 @@
-import './App.css'
-import Header from "./components/header"
-import Hero from "./components/hero"
-import Projects from "./components/projects"
-import Footer from "./components/footer"
-import Contact from "./components/contact"
-import About from "./components/about"
+import './App.css';
+import Header from "./components/header";
+import Hero from "./components/hero";
+import Projects from "./components/projects";
+import Footer from "./components/footer";
+import Contact from "./components/contact";
+import About from "./components/about";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
 function App() {
-
   return (
-    <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </>
-  )
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
