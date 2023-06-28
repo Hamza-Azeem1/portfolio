cd24d5c93a1ca2c249a19e32ff9a473d81a34dfc
Hamza-Azeem1 <hamzaazeem023@gmail.com>
2023-06-28 15:37:45 +0500

    Projects added

--- src/components/data/projectsData.jsx
+++ src/components/data/projectsData.jsx
@@ -1,4 +1,11 @@
 import img1 from '../../assets/Project Images/jsStopwatch.png'
+import img2 from '../../assets/Project Images/tours.png'
+import img3 from '../../assets/Project Images/accordion.png'
+import img4 from '../../assets/Project Images/cart.png'
+import img5 from '../../assets/Project Images/color-generator.png'
+import img6 from '../../assets/Project Images/grocery.png'
+import img7 from '../../assets/Project Images/menu.png'
+import img8 from '../../assets/Project Images/slider.png'
 
 const projects = [
     {
@@ -8,6 +15,62 @@ const projects = [
         codeLink: 'https://github.com/Hamza-Azeem1/StopWatch-JS.git',
         description: 'Supercharge your project development with our robust lab.',
         title: 'Javascript StopWatch',
+    },
+    {
+        id: 2,
+        imageSrc: img2,
+        liveLink: 'https://react-project2-tours.netlify.app',
+        codeLink: 'https://github.com/Hamza-Azeem1/React-tours.git',
+        description: 'Supercharge your project development with our robust lab.',
+        title: 'Tours Site',
+    },
+    {
+        id: 3,
+        imageSrc: img3,
+        liveLink: 'https://react-project3-accordion.netlify.app',
+        codeLink: 'https://github.com/Hamza-Azeem1/React-accordion.git',
+        description: 'Supercharge your project development with our robust lab.',
+        title: 'Accordion',
+    },
+    {
+        id: 4,
+        imageSrc: img4,
+        liveLink: 'https://react-project7-grocery.netlify.app',
+        codeLink: 'https://github.com/Hamza-Azeem1/React-cart.git',
+        description: 'Supercharge your project development with our robust lab.',
+        title: 'Cart',
+    },
+    {
+        id: 5,
+        imageSrc: img5,
+        liveLink: 'https://react-project6-colorgenerator.netlify.app',
+        codeLink: 'https://github.com/Hamza-Azeem1/React-ColorGenerator.git',
+        description: 'Supercharge your project development with our robust lab.',
+        title: 'Color Generator',
+    },
+    {
+        id: 6,
+        imageSrc: img6,
+        liveLink: 'https://react-project8-cart.netlify.app',
+        codeLink: 'https://github.com/Hamza-Azeem1/React-reducer.git',
+        description: 'Supercharge your project development with our robust lab.',
+        title: 'Item Reducer',
+    },
+    {
+        id: 7,
+        imageSrc: img7,
+        liveLink: 'https://react-projetc4-menu.netlify.app',
+        codeLink: 'https://github.com/Hamza-Azeem1/React-menu.git',
+        description: 'Supercharge your project development with our robust lab.',
+        title: 'Menu',
+    },
+    {
+        id: 8,
+        imageSrc: img8,
+        liveLink: 'https://react-project5-slider.netlify.app',
+        codeLink: 'https://github.com/Hamza-Azeem1/React-slider.git',
+        description: 'Supercharge your project development with our robust lab.',
+        title: 'Slider',
     },
 ];
 export default projects;
